6a8a36aecc4d70d4594ff041dec4e183922c7b2f
Andrezza Souza <andrezzasouza@id.uff.br>
2023-03-22 21:35:28 -0300

    feat: ask for a number and show its multiplication table

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Escolha um número para ver sua tabela de multiplicação:");
+        String stringNum = userInput.nextLine();
+        int num = Integer.parseInt(stringNum);
+
+        System.out.println("Tabela de multiplicação de " + num);
+        
+        for (int i = 0; i < 11; i++) {
+            int product = num * i;
+            System.out.println(num + " x " + i + " = " + product);
+        }
     }
 }
